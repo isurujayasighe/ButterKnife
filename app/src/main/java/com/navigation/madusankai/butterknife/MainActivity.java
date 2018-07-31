545d44491f40cbdc554cf78e26f62e2062c934f9
Madusanka I. Jayasinghe <Madusankai@openarc.net>
2018-07-31 08:34:25 +0530

    Initial commit

---
+++ app/src/main/java/com/navigation/madusankai/butterknife/MainActivity.java
@@ -0,0 +1,38 @@
+package com.navigation.madusankai.butterknife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.testText)
+    TextView title;
+
+    @BindView(R.id.edit_text)
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        title.setText("Hello ButterKnife");
+    }
+
+    @OnClick(R.id.button)
+    public void submit() {
+        Toast.makeText(MainActivity.this,"Hello from Butterknife OnClick annotation", Toast.LENGTH_SHORT).show();
+    }
+
+   @OnClick(R.id.submit_button)
+    public void submitbutton(){
+       Toast.makeText(MainActivity.this,"Hello from Butterknife OnClick annotation", Toast.LENGTH_SHORT).show();
+   }
+}
